a2b6f6b1bd92f4f1ea8a26d57173e7d11ee50f80
Thomas Watson Steen <w@tson.dk>
2014-10-16 23:07:38 +0800

    Add proper HTTP headers

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 var fs = require('fs');
 var path = require('path');
-var script = fs.readFileSync(path.join(__dirname, 'teapot.min.js'));
+var body = '<script>' + fs.readFileSync(path.join(__dirname, 'teapot.min.js')) + '</script>';
 
 module.exports = function (req, res) {
-  res.writeHead(418);
-  res.end('<script>' + script + '</script>');
+  res.writeHead(418, {
+    'Content-Length': Buffer.byteLength(body),
+    'Content-Type': 'text/html; charset=utf-8'
+  });
+  res.end(body);
 };
